e22c6e7d9088f91ff4cf8831ffd39af2c732cd74
yordanos-bogale5 <bogaleyordanos64@gmail.com>
2024-01-30 14:43:36 +0300

    app

---
+++ app/src/main/java/com/project/stbc/MainActivity.java
@@ -0,0 +1,26 @@
+package com.tadekaldas.stbc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.window.SplashScreen;
+
+public class MainActivity extends AppCompatActivity {
+
+    Handler h = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent (getApplicationContext(), Home.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
